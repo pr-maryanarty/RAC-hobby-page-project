310cd4d354c08f0c01961be558de43161f1a53f9
pr-maryanarty <maryana.dementeva@gmail.com>
2023-02-02 16:11:54 +0000

    introduced a date() into the page

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
-const date = new Date();
-document.getElementById("today").innerHTML;
+let now = new Date();
+let date = now.getDate();
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+document.querySelector("#today").innerHTML = `${month} ${date} `;
